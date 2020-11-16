6ab9ed2945881e8287bc95a8a25ea8960dda363b
iviczian <viczian.istvan@gmail.com>
2020-11-16 13:53:25 +0100

    Session scope

--- src/main/java/training/employee/HelloController.java
+++ src/main/java/training/employee/HelloController.java
@@ -2,20 +2,25 @@ package training.employee;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import java.time.LocalDateTime;
 
 @Component
-@RequestScope
+@SessionScope
 public class HelloController {
 
     private String message = "Type your name!";
 
     private String name = "Jack Doe";
 
-    public void sayHello() {
+    public String sayHello() {
         message = "Hello " + name + " " + LocalDateTime.now();
         name = "";
+        return "/hello.xhtml?faces-redirect=true";
     }
 
     public String getMessage() {
