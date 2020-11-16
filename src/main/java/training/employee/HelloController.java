581851fdb7fb49e392215a7472e7b7d81effe429
iviczian <viczian.istvan@gmail.com>
2020-11-16 11:53:24 +0100

    Form

--- src/main/java/training/employee/HelloController.java
+++ src/main/java/training/employee/HelloController.java
@@ -9,7 +9,24 @@ import java.time.LocalDateTime;
 @RequestScope
 public class HelloController {
 
+    private String message = "Type your name!";
+
+    private String name = "Jack Doe";
+
+    public void sayHello() {
+        message = "Hello " + name + " " + LocalDateTime.now();
+        name = "";
+    }
+
     public String getMessage() {
-        return "Hello World " + LocalDateTime.now();
+        return message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
